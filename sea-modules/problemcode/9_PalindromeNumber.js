dd74b284a69d78704a4319c02dd8a8df4a4ae41b
Hachi <swordarchor@gmail.com>
2015-07-05 21:54:15 +0800

    x

---
+++ sea-modules/problemcode/9_PalindromeNumber.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+define(function(require, exports, module) {
+  var isPalindrome = function(x) {
+
+  };
+  exports.isPalindrome = isPalindrome;
+});
--- sea-modules/tests.js
+++ sea-modules/tests.js
@@ -2,6 +2,23 @@ define(function(require) {
 
   var qunit = require('qunit');
 
+  //Problem 9
+  var problem9 = require('problem9');
+
+  QUnit.test("leetcode test - problem 9", function(assert) {
+    var paras = [
+      { args: undefined, expected: null },
+    ];
+
+    paras.forEach(function(para) {
+      var actual = problem9.isPalindrome(para.args);
+      assert.equal(
+        actual,
+        para.expected,
+        para.args + " expects: " + actual + " equal to " + para.expected);
+    });
+  });
+
   //Problem 8
   var problem8 = require('problem8');
 
@@ -30,25 +47,22 @@ define(function(require) {
     });
   });
 
-  //Problem 4
-  var problem4 = require('problem4');
+  //Problem 7
+  var problem7 = require('problem7');
 
-  QUnit.test("leetcode test - Problem 4", function(assert) {
+  QUnit.test("leetcode test - Problem 7", function(assert) {
     var paras = [
-      { args1: undefined, args2: undefined, expected: null }, 
-      { args1: [1, 2, 2], args2: [], expected: 2 }, 
-      { args1: [1, 2, 2], args2: [1, 2, 3], expected: 2 }, 
-      { args1: [1, 1], args2: [2, 2, 2, 3], expected: 2 }, 
-      { args1: [1, 3, 4], args2: [1, 2, 5], expected: 2.5 }, 
-      { args1: [0, 3, 4], args2: [1, 2, 5, 8, 9], expected: 3.5 }, 
-      { args1: [1, 2], args2: [1, 1], expected: 1 }, 
-      { args1: [1], args2: [1], expected: 1 }, 
-      { args1: [1, 1], args2: [1, 2], expected: 1 }, 
-      { args1: [0, 0, 1, 1], args2: [0, 0, 1, 2], expected: 0.5 }
+      { args: undefined, expected: null }, 
+      { args: 1, expected: 1 }, 
+      { args: -1, expected: -1 }, 
+      { args: 123, expected: 321 }, 
+      { args: -123, expected: -321 }, 
+      { args: 1000, expected: 1 }, 
+      { args: 1534236469, expected: 0 }
     ];
 
     paras.forEach(function(para) {
-      var actual = problem4.findMedianSortedArrays(para.args1, para.args2);
+      var actual = problem7.reverse(para.args);
       assert.equal(
         actual,
         para.expected,
@@ -78,22 +92,25 @@ define(function(require) {
     });
   });
 
-  //Problem 7
-  var problem7 = require('problem7');
+  //Problem 4
+  var problem4 = require('problem4');
 
-  QUnit.test("leetcode test - Problem 7", function(assert) {
+  QUnit.test("leetcode test - Problem 4", function(assert) {
     var paras = [
-      { args: undefined, expected: null }, 
-      { args: 1, expected: 1 }, 
-      { args: -1, expected: -1 }, 
-      { args: 123, expected: 321 }, 
-      { args: -123, expected: -321 }, 
-      { args: 1000, expected: 1 }, 
-      { args: 1534236469, expected: 0 }
+      { args1: undefined, args2: undefined, expected: null }, 
+      { args1: [1, 2, 2], args2: [], expected: 2 }, 
+      { args1: [1, 2, 2], args2: [1, 2, 3], expected: 2 }, 
+      { args1: [1, 1], args2: [2, 2, 2, 3], expected: 2 }, 
+      { args1: [1, 3, 4], args2: [1, 2, 5], expected: 2.5 }, 
+      { args1: [0, 3, 4], args2: [1, 2, 5, 8, 9], expected: 3.5 }, 
+      { args1: [1, 2], args2: [1, 1], expected: 1 }, 
+      { args1: [1], args2: [1], expected: 1 }, 
+      { args1: [1, 1], args2: [1, 2], expected: 1 }, 
+      { args1: [0, 0, 1, 1], args2: [0, 0, 1, 2], expected: 0.5 }
     ];
 
     paras.forEach(function(para) {
-      var actual = problem7.reverse(para.args);
+      var actual = problem4.findMedianSortedArrays(para.args1, para.args2);
       assert.equal(
         actual,
         para.expected,
